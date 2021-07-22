988f917a4e8c6e8c09c55b094c2b3768b6ebc916
DibrajSarkar <hi.dibraj@gmail.com>
2021-07-22 20:42:21 +0600

    Variable in C

---
+++ Variable.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{   /* integer variable
+    double variable
+    floating variable
+    long long integer
+    character
+    boolean
+
+    */
+    int y;// variable declearization
+    int a=10; // variable declearization and intialaization
+    printf(" Value of a is : %d\n",a);
+    //%d is placeholder, which is hold another value
+    float x=10.12;
+    // placeholder for float is %f
+    printf("Floating Value is : %f\n",x);
+    double f=10.12343;
+    //placeholder for double is %lf
+    printf(" Value of Double: %lf\n",f);
+
+    char charac='A';
+    // Placeholder for char is %c;
+    printf(" character is : %c\n",charac);
+
+    return 0;
+
+}
