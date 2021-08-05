ed19bb58e3ce4066c71ba011b4fb0c4a4c4425aa
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-05 12:01:41 +0600

    Operator

--- Operator.c
+++ Operator.c
@@ -10,12 +10,21 @@ int main(){
     int x=5;
     printf("value of x: %d\n",x++);// x=5, increse 6,print 5..x++ first value then increment 
     printf("Value of x:%d\n",++x); // ++x ,6+1=7, printf 7, ++x first increment value than print
-    int x=5;
-    printf("Value of x :%d\n",++x);// 6
-    printf("Value of x: %d\n",x++);// 6 ,increment it 7, but not print , after another line it will print.
+    int u=5;
+    printf("Value of x :%d\n",++u);// 6
+    printf("Value of x: %d\n",u++);// 6 ,increment it 7, but not print , after another line it will print.
 
     printf("Value of a: %d\n",a);
     printf("Value of b: %d\n",b);
+
+    //if(5>10-5 && 5)// 5>5 && 5, 0 && 5, false
+    if(1 == 2 && ! = 0)
+    {
+        printf("Hi\n");
+    }
+    else{
+        printf("Hello\n");
+    }
 
     return 0;
 }
