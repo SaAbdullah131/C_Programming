8d2002f19c8fcb88da7d94205d72f66d3a115fa2
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-06 16:28:59 +0600

    While Loop in C Programming

---
+++ WhileLoop.c
@@ -0,0 +1,16 @@
+// While Loop in C Programming
+#include<stdio.h>
+int main()
+{
+    int x=1;
+    while (x<=5)
+    {
+        printf("Hi\n");
+        printf("Hello\n");
+        x+=2;
+    }
+    
+
+
+    return 0;
+}
