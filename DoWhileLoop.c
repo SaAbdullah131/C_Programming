d5c5940c40b8574420389c551b601a3df227e93d
DibrajSarkar <hi.dibraj@gmail.com>
2021-08-09 00:56:36 +0600

    Do While Loop in C

--- DoWhileLoop.c
+++ DoWhileLoop.c
@@ -11,6 +11,14 @@ int main()
     } while (i<=2){
         printf("%d\n",i);
     }
+    int a=100;
+    do
+    {
+        printf("%d\n");
+        i+=5;
+    } while (i<=0){
+        printf("%d\n");
+    }
     
     // Do while loop .First Do and then check
     return 0;
