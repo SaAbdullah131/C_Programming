ea8ac3cc90c6733bb0de1afb0df779f87af7a00d
SaAbdullah131 <hi.dibraj@gmail.com>
2021-08-23 23:20:09 +0600

    Nested Loop in C Programming

---
+++ NestedLoop.c
@@ -0,0 +1,18 @@
+// Nested Loop in C Programming. When one loop stay inside of another loop it's siuation called Nested Looping.
+
+#include<stdio.h>
+int main(){
+    int i,j;
+    for ( i = 0; i < count; i++)
+    {
+        for ( j = 0; j < count; j++)
+        {
+            printf("");
+        }
+        
+    }
+    
+
+
+    return 0;
+}
