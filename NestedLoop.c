00747785f5a804c271e76ca74a5f93adea775c59
SaAbdullah131 <hi.dibraj@gmail.com>
2021-08-24 00:59:41 +0600

    Nested Loop in C

--- NestedLoop.c
+++ NestedLoop.c
@@ -3,9 +3,32 @@
 #include<stdio.h>
 int main(){
     int i,j;
-    for ( i = 0; i <=5; i++)
+    for ( i = 1; i <=3; i++)
     {
-        for ( j = 0; j <=i; j++)
+        printf("Outside of the Loop\n");
+        for ( j = 1; j <=2; j++)
+        {
+            printf("Inside of Loop\n");
+        }
+     
+        
+    }
+    for (int k = 1; k <=2; k++)
+    {
+        for (int l = 1; l <=3; l++)
+        {
+            for (int m = 1; m <=5; m++)
+            {
+                printf("Hi-\n");
+            }
+            
+        }
+        
+    }
+    
+    for (int q =1; q<=5; q++) // for line printing
+    {
+        for (int w = q; w <=5; w++) // for star printing
         {
             printf("*");
         }
@@ -13,7 +36,6 @@ int main(){
         
     }
     
-
 
     return 0;
 }
