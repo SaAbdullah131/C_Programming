fff7c102c4416d387302d28ac3353c87eb35e6d5
SaAbdullah131 <hi.dibraj@gmail.com>
2021-09-14 01:45:22 +0600

    Array in C Programming

---
+++ Array01.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    int a[10],i;
+    int sum=0;
+
+    for ( i = 0; i <10; i++)
+    {
+        scanf("%d",&a[10]);
+    }
+    
+    for ( i = 0; i <10; i++)
+    {
+        sum=sum+a[i];
+    }
+    
+    printf(" Sum of Array is : %d\n",sum);
+
+
+    return 0;
+}
