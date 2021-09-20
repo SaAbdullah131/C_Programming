f1c3e2fbbf815c9ab08b99a9c7ee7296dd9ae06b
SaAbdullah131 <hi.dibraj@gmail.com>
2021-09-20 15:01:03 +0600

    Two Dimensional Array in C Programming

---
+++ 2DArray1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int a[3][3],i,j,sum=0;
+    printf("Enter The Value of Array: \n");
+
+    for ( i = 0; i < 3; i++)// for row
+    {
+        for ( j = 0; j < 3; j++)// for coloum
+        {
+            scanf("%d",&a[i][j]);
+        }
+        
+    }
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf( "%d ",a[i][j]);
+            sum=sum+a[i][j];
+        }
+        printf("\n");
+        
+    }
+    float average=sum/9.0;
+    printf("Average is: %.2f\n",average);
+
+     return 0;
+}
