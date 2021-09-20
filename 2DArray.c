c779f65a891bd5b8fcb54be15a1359b9de2b2bce
SaAbdullah131 <hi.dibraj@gmail.com>
2021-09-20 14:43:31 +0600

    Two Dimensional Array in C Programming

--- 2DArray.c
+++ 2DArray.c
@@ -1,9 +1,12 @@
 // Two dimensional Array
 #include<stdio.h>
 int main(){
-    int x[2][3]={11,12,13,21,22,23};
-     printf("%d\n",x[1][2]);
-
+    int x[2][3]={11,12,13,
+                21,22,23};
+     printf("%d\n",x[1][2]); // row 1 coloum 2
+     
+    int y=x[0][0]+x[1][1];
+    printf("%d\n",y);
 
 
     return 0;
