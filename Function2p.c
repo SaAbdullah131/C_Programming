86c026bda730fff0c63c8d7cd13bfd7e7f5fdeb8
SaAbdullah131 <hi.dibraj@gmail.com>
2021-09-22 23:31:17 +0600

    Function simple problem

---
+++ Function2p.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void EvenOrOdd(int n){
+    if (n%2==0)
+    {
+        printf("Even\n");
+    }
+    else{
+        printf("Odd");
+    }
+
+
+}
+int main(){
+
+
+    return 0;
+}
