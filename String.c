b74896c816df5b1df42628c626e73488f3798d34
SaAbdullah131 <hi.dibraj@gmail.com>
2021-10-11 12:50:24 +0600

    String in c Programming

---
+++ String.c
@@ -0,0 +1,10 @@
+// A sequential collection of character or Character Array terminated by a null character ('0\')
+#include<stdio.h>
+int main(){
+
+char a[]={}
+
+
+}
+
+// char my_char[50];capacity of storing 49 character  plus a null character
