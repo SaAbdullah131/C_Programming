d281433ce751604edaa362d4515cd0f2ea3859f4
SaAbdullah131 <hi.dibraj@gmail.com>
2021-10-24 12:02:27 +0600

    String strcmp function in c programming

--- StringStrcmp.c
+++ StringStrcmp.c
@@ -2,10 +2,19 @@
 #include<string.h>
 int main(){
 
+char a[]="Bangladesh"; // if both value is dhaka them it will be equal.
+char b[]="Dhaka";
 
+int x =strcmp(a,b);
 
-
-
+if (x==0)
+{
+    printf("a and b is equal.\n");
+}
+else
+{
+    printf("a and b is not equal.\n");
+}
 
 
     return 0;
