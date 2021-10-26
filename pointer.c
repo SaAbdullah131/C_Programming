fe9ae41eeba1ab431c6c75608e5dc7d6dfd7613e
SaAbdullah131 <hi.dibraj@gmail.com>
2021-10-26 13:09:32 +0600

    Pointer in C programming

--- pointer.c
+++ pointer.c
@@ -1,13 +1,30 @@
 #include<stdio.h>
 int main(){
     printf("Pointer in C : Introduction\n");
-    int x=10;
+    int a=10;
     int *p;
-    p=&x;
-    printf("Value of x: %d\n",x);
-    printf("Value of x: %d\n",*p);
+    p=&a;
+    // printf("Value of x: %d\n",x);
+    // printf("Value of x: %d\n",*p);
 
+int x,y,z;
+x=10,y=250;z=30;
+int *p1,*p2,*p3;
+p1=&x;
+p2=&y;
+p3=&z;
 
+if (*p1>*p2 && *p1>*p3){
+    printf("The largest number is :%d\n",*p1);
+}
+else if (*p2>*p1 && *p2>*p3)
+{ 
+    printf("The largest Number is : %d\n",*p2);
+
+}
+
+else
+    printf("The largest number is :%d\n",*p3);
 
 
     return 0;
