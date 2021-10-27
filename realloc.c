936b1f1ae8d32b15352451b787ab1ec388f06939
SaAbdullah131 <hi.dibraj@gmail.com>
2021-10-27 19:41:24 +0600

    Memory reallocation concept in c programming

--- realloc.c
+++ realloc.c
@@ -3,8 +3,18 @@
 int main(){
 
     int *ptr1,*ptr2;
-    ptr1=
+    ptr1=(int *)malloc(5*sizeof(int));
+    ptr2=(int *)calloc(5,sizeof(int));
 
+    if (ptr1==NULL || ptr2==NULL)
+    {
+        printf("Memory not allocated.\n");
+    }
+    else{
+        printf("Memory allocation is successfull.\n");
+        ptr1=realloc(ptr1,50);// before size(20) not matter now 50
+        printf("Memory reallocation successfull\n");
+    }
 
     return 0;
 }
